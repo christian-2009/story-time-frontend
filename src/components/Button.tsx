6195aaf0b35e2b11d6c0a16d6518aad8a8f1cc74
Christian <christian.avione.io>
2023-04-13 22:40:20 +0100

    created chat functionality

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,20 @@
+import { ReactEventHandler } from "react";
+
 interface Props {
   text: string;
   optionalStyles?: React.CSSProperties;
   type?: "button" | "submit" | "reset" | undefined;
+  onClick?: () => any;
 }
 
-export default function Button({ text, optionalStyles, type }: Props) {
+export default function Button({ text, optionalStyles, type, onClick }: Props) {
   return (
-    <button className="login-button" style={optionalStyles} type={type}>
+    <button
+      className="login-button"
+      style={optionalStyles}
+      type={type}
+      onClick={onClick}
+    >
       {text}
     </button>
   );
