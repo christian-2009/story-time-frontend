5312224e74507f502aab89c4370edebf2f7fc5f9
Christian <christian.avione.io>
2023-05-03 17:29:13 +0100

    trying things

--- src/socket.ts
+++ src/socket.ts
@@ -2,7 +2,7 @@ import * as io from "socket.io-client";
 
 const URL =
   process.env.NODE_ENV === "production"
-    ? "https://story-time-2swz.onrender.com"
+    ? "https://story-time-backend.vercel.app"
     : "http://localhost:6379";
 
 // const URL = "https://story-time-2swz.onrender.com";
